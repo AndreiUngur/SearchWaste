a72131b7f9f178a8840f2ddc85808acf3ff71a5f
Andrei <andrei.ungur@mail.mcgill.ca>
2019-01-10 11:50:34 -0500

    Updates

---
+++ precache-manifest.063488e9528645cb1e36d5a92bf26a78.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "04edf1b297bd99c7f89b",
+    "url": "/SearchWaste/static/css/main.7daf96f7.chunk.css"
+  },
+  {
+    "revision": "04edf1b297bd99c7f89b",
+    "url": "/SearchWaste/static/js/main.04edf1b2.chunk.js"
+  },
+  {
+    "revision": "70324af9603ffc4b2af4",
+    "url": "/SearchWaste/static/css/1.0bfaab5c.chunk.css"
+  },
+  {
+    "revision": "70324af9603ffc4b2af4",
+    "url": "/SearchWaste/static/js/1.70324af9.chunk.js"
+  },
+  {
+    "revision": "bb51dfb25a5c0832e454",
+    "url": "/SearchWaste/static/js/runtime~main.bb51dfb2.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SearchWaste/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SearchWaste/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SearchWaste/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SearchWaste/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SearchWaste/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "38b4df1fc2392f1bb639a5f586bdb30b",
+    "url": "/SearchWaste/index.html"
+  }
+];
