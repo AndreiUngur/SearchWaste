32221a01492c02b18ece921329db2a7d515b5bdd
Andrei <andrei.ungur@mail.mcgill.ca>
2019-01-10 11:41:10 -0500

    Add favorites + search function

--- searchwaste/src/App.js
+++ searchwaste/src/App.js
@@ -18,14 +18,53 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      searchResults: [],
+      searchText: ""
     };
   }
 
   componentDidMount() {
-    fetch(`https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=10`)
+    fetch(`https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000`)
     .then(result=>result.json())
-    .then(items=>this.setState({data: items}))
+    .then(items=>this.setState({data: items.map((i, index) => {i.index = index; return i;})}))
+  }
+
+  handleClick(e, i){
+    if (this.state.data[i.index].favorite){ 
+      this.state.data[i.index].color = "#000000";
+      this.state.data[i.index].favorite = false;
+    } else {
+      this.state.data[i.index].color = "#237e7f";
+      this.state.data[i.index].favorite = true;     
+    }
+    this.forceUpdate();
+  }
+
+  validateText(keywords, text){
+    return keywords.split(", ").some(i => text.includes(i)) || text.split(" ").some(i => keywords.includes(i))
+  }
+
+  handleSecondClick(e){
+    if (this.state.searchText === ""){
+      return;
+    }
+
+    this.setState({searchResults: this.state.data.filter(i => this.validateText(i.keywords, this.state.searchText))});
+  }
+
+  handleChange(event){
+    this.setState({searchText: event.target.value});
+    if (event.target.value === ""){
+      this.setState({searchResults: []});
+    }
+  }
+
+  renderIfFavorites(){
+    if (this.state.data.filter(i => i.favorite).length > 0){
+      return "Favorites"
+    }
+    return "None"
   }
 
   render() {
@@ -35,21 +74,35 @@ class App extends Component {
         Toronto Waste Lookup
         </header>
         <div className="App-searchbar">
-          <input text className="App-searchbox"></input>
-          <Button type="submit" variant="contained" style={{backgroundColor: "#237e7f", fontSize: '24px'}}>
+          <input text value={this.state.searchText} onChange={(e) => this.handleChange(e)} className="App-searchbox"></input>
+          <Button type="submit" onClick={(e) => this.handleSecondClick(e)} variant="contained" style={{backgroundColor: "#237e7f", fontSize: '24px'}}>
             <i class="fa fa-search icon-flipped"></i>
           </Button>
         </div>
         <div className="App-results">
-        {this.state.data.map(function(i){
+        {this.state.searchResults.map(i => {
           return <div class="row result">
                   <div class="col-left">
-                    <Button><i class="fa fa-star"></i></Button>
+                    <Button onClick={(e) => this.handleClick(e, i)} style={{fontSize: '24px', color: i.color}}><i class="fa fa-star"></i></Button>
                     <span class="title">{i.title}</span>
                   </div>
                   <div class="col-right" dangerouslySetInnerHTML={{ __html: escapeHtml(i.body) }}></div>
                 </div>
-        })}
+          }, this)
+        }
+        </div>
+        <div className="App-favorites">
+          <h1 className="App-favorites-header">{this.state.data.filter(i => i.favorite).length === 0 ? "" : "Favorites"}</h1>
+        {this.state.data.filter(i => i.favorite).map(i => {
+          return <div class="row result">
+                  <div class="col-left">
+                    <Button onClick={(e) => this.handleClick(e, i)} style={{fontSize: '24px', color: i.color}}><i class="fa fa-star"></i></Button>
+                    <span class="title">{i.title}</span>
+                  </div>
+                  <div class="col-right" dangerouslySetInnerHTML={{ __html: escapeHtml(i.body) }}></div>
+                 </div>
+          })
+        }
         </div>
       </div>
     );
